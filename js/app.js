f5a9897d6761a6d33e2187b7c3b92ed209410eae
khanshorif331 <khanshorif331@gmail.com>
2022-03-13 01:38:22 +0600

    dark mode implemented

--- js/app.js
+++ js/app.js
@@ -82,3 +82,22 @@ const updateTotal = () => {
   return grandTotal
 };
 // loadProducts();
+
+
+// dark mode implement through darkmode js
+
+const options ={
+  bottom: '64px', // default: '32px'
+  right: '32px', // default: '32px'
+  left: 'unset', // default: 'unset'
+  time: '0.3s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: false, // default: true,
+  label: '🌓', // default: ''
+  autoMatchOsTheme: true // default: true 
+}
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
